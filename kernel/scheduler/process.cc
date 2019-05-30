97e005e5dafb61de60562514c18d3bf1545f6b0c
vic-lee <viclee@nyu.edu>
2019-05-29 21:59:45 -0400

    Initialize state

--- kernel/scheduler/process.cc
+++ kernel/scheduler/process.cc
@@ -3,5 +3,5 @@
 namespace scheduler
 {
 process_t::process_t(pid_t pid, size_t psize, size_t priority, bool sys)
-    : sys(sys), pid(pid), ppid(0), psize(psize), priority(priority) {}
+    : state(READY), sys(sys), pid(pid), ppid(0), psize(psize), priority(priority) {}
 }
