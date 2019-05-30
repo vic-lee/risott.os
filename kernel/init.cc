c3becbce1d348e02a739e3e996876f13d8ade213
vic-lee <viclee@nyu.edu>
2019-05-29 21:18:22 -0400

    Update init

--- kernel/init.cc
+++ kernel/init.cc
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 #include "config.h"
+#include "scheduler/procmngr.h"
 
 int main(int argc, char *argv[])
 {
     kernel::Config cfg = kernel::Config();
+    scheduler::ProcessManager pmg = scheduler::ProcessManager();
+    pmg.load_new_process();
 }
